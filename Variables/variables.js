d636011c32257c191487561266038d7e5c6cd9f5
Sahithya D <78305502+sahithyajain@users.noreply.github.com>
2026-08-10 21:27:18 +0530

    Update variables.js

--- Variables/variables.js
+++ Variables/variables.js
@@ -1,1 +1,81 @@
+// 1. VAR - Program 1
 
+
+var city = "Bangalore";
+console.log(city);
+
+// Output:Bangalore
+
+// How it works:  var creates a variable called city and stores "Bangalore" in it.
+
+
+// --------------------------
+// 2. VAR - Program 2
+// ----------------------------
+
+var number = 10;
+number = 20;
+console.log(number);
+
+// Output: 20
+
+// How it works: var allows the value of a variable to be changed.
+
+
+// -----------------------------
+// 3. LET - Program 1
+//---------------------------------
+
+let age = 22;
+console.log(age);
+
+// Output: 22
+
+// How it works: let creates a variable called age and stores 22 in it.
+
+
+// =====================================
+// 4. LET - Program 2
+// =====================================
+
+let score = 50;
+
+score = 80;
+
+console.log(score);
+
+// Output:
+// 80
+
+// How it works:
+// let allows us to change the value stored in the variable.
+
+
+// =====================================
+// 5. CONST - Program 1
+// =====================================
+
+const country = "India";
+
+console.log(country);
+
+// Output:
+// India
+
+// How it works:
+// const creates a variable whose value cannot be reassigned.
+
+
+// =====================================
+// 6. CONST - Program 2
+// =====================================
+
+const language = "JavaScript";
+
+console.log(language);
+
+// Output:
+// JavaScript
+
+// How it works:
+// const stores a value that should not be reassigned.
